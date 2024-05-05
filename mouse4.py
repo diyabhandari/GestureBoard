944e09aa904105de9096c736b83d17f9fc55fcba
Diya Bhandari <diyabhandari@gmail.com>
2024-05-05 21:09:20 +0530

    restrict mouse activation

--- mouse4.py
+++ mouse4.py
@@ -1,17 +1,23 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector #pip install cvzone (see github repo)
 import mouse #pip install mouse
-import numpy
+from numpy import interp
 from win32api import GetSystemMetrics
 
 detector = HandDetector(detectionCon=0.9,maxHands=1) ##detection confidence
 
 cap = cv2.VideoCapture(0)
+cap_w = 640
+cap_h = 480
+cap.set(3,cap_w) #3-> width 4->height of video capture object
+cap.set(4,cap_h)
+frameR = 50 #to solve jittering near the edges of the display window, we'll reduce its size by this much
 while True:
   success,image = cap.read()
   image = cv2.flip(image,1)
   ##extract all information regarding our hands. right or left hand ? processed image
   hands, image = detector.findHands(image,flipType=False) #we alr flipped with cv2
+  cv2.rectangle(image,(frameR,frameR),(cap_w-frameR,cap_h-frameR),(255,0,255),2) ##visualize region in which mouse gesture is detected
 
   ##we will use the INDEX_FINGER_TIP, 8, for the mouse
   #if hand is present in frame 
@@ -20,13 +26,21 @@ while True:
     ind_x,ind_y = lmList1[8][0],lmList1[8][1] #8 - > index finger, 0 and 1 give coordinates
     #encircle tip to highlight it
     cv2.circle(image,(ind_x,ind_y),5,(0,255,255),2)
-    #conv coordinates of tip of index finger wrt screen size of the device
-    conv_x = int(numpy.interp(ind_x,(0,cap.get(3)),(0,GetSystemMetrics(0))))
-    #3-> width 4->height of video capture object
-    conv_y = int(numpy.interp(ind_y,(0,cap.get(4)),(0,GetSystemMetrics(1))))
+    fingers = detector.fingersUp(hands[0])
+    #print(fingers)
+    #we want only the index finger to be up for the mouse to activated 
+    #because we flipped the livestream, it examines the left hand as it would the right, when thumb is open itll detect it as closed
+    #indexing of fingers array starts from thumb
 
-    ##pass new coordinates 
-    mouse.move(conv_x,conv_y)
+    if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 0:
+      #didnt specify for 3,4 cuz you cant have 2 closed but those oepn
+      #conv coordinates of tip of index finger wrt screen size of the device
+      conv_x = int(interp(ind_x,(frameR,cap_w-frameR),(0,GetSystemMetrics(0))))
+      conv_y = int(interp(ind_y,(frameR,cap_h-frameR),(0,GetSystemMetrics(1))))
+      ##pass new coordinates 
+      mouse.move(conv_x,conv_y)
+
+
   cv2.imshow("mouse4",image)
   key = cv2.waitKey(100)
   if key ==27:
