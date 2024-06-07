1573143a3c24496c8cb22263ecf09ec8d1d5eefe
Diya Bhandari <diyabhandari@gmail.com>
2024-06-07 12:49:02 +0530

    fix volume up/down pose correction

--- volume.py
+++ volume.py
@@ -12,19 +12,20 @@ def volume_control():
       lmList1 = hands[0]['lmList'] #store all 21 landmarks of the first hand detected in a list
       #thumbd up -> volume up, will turn up volume as long as that thumb is up, in increments of 1-2 ?
       #similar for thumbs down, but must learn how to detect thumbs up and down separately
-
+##################################################################################
       #store coordinates of thumb
       thumb_x = lmList1[4][0]
       thumb_y= lmList1[4][1]
       fingers = detector.fingersUp(hands[0]) # fingers that are up or down in the first hand that appears on cam
       if fingers[0]==0 and fingers[1]==0 and fingers[2]==0 and fingers[3]==0 and fingers[4]==0:
-        cv2.circle(image,(thumb_x,thumb_y),5,(0,255,255),2)
-
-    
-    cv2.imshow("mouse",image)
+        print("volume up")
+      if fingers[0]==1 and fingers[1]==0 and fingers[2]==0 and fingers[3]==0 and fingers[4]==0: #thumb flipped, 1 -> its down
+        print("volume down")  
+    cv2.imshow("GestureBoard",image)
     key = cv2.waitKey(100)
     if key ==27:
       break
 
   cap.release()
   cv2.destroyAllWindows()
+volume_control()
