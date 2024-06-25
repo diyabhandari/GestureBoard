bcc3bcbec615bf6caa4b61df45db73ed4fe2f730
Diya Bhandari <diyabhandari@gmail.com>
2024-06-25 22:50:31 +0530

    add time delay in between screenshots

--- screenshot.py
+++ screenshot.py
@@ -2,10 +2,13 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import pyautogui
 from datetime import datetime
+import time
 
 def screenshot_func():
     detector = HandDetector(detectionCon=0.6, maxHands=1)
     cap = cv2.VideoCapture(0)
+    ss_delay=0
+    ss_delay_duration=2
     while True:
         success, image = cap.read()
         image = cv2.flip(image, 1)
@@ -15,10 +18,13 @@ def screenshot_func():
             lmList = hands[0]['lmList']
             # Screenshot gesture
             if fingers == [1, 1, 1, 1, 0]:  # first 4 fingers up
-                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-                filename = f"image_{timestamp}.png"
-                im1 = pyautogui.screenshot(filename)
-                print(f"Screenshot taken: {filename}")
+                current_time = time.time()
+                if current_time - ss_delay >= ss_delay_duration:
+                    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+                    filename = f"image_{timestamp}.png"
+                    im1 = pyautogui.screenshot(filename)
+                    print(f"Screenshot taken: {filename}")
+                    ss_delay = current_time
         cv2.imshow("GestureBoard", image)
         key = cv2.waitKey(100)
         if key == 27:  # ESC key to exit
